d3662937d158da43ffdb0e0d2bf1bc6599863992
Jesús Mejía <jesusmejded@gmail.com>
2019-12-06 14:24:55 -0600

    naive bayes is working

--- main.jl
+++ main.jl
@@ -1,6 +1,6 @@
 using RDatasets
-
-include("dicretize.jl")
+import Random: randperm
+include("discretize.jl")
 include("nets.jl")
 include("inference.jl")
 
@@ -8,30 +8,39 @@ include("inference.jl")
 function classification()
     iris = dataset("datasets", "iris")
     data = discret_df(iris)
+    n_instances = size(data, 1)
+
+    I = randperm(n_instances)
+
+    n_train = round(Int, 0.6*n_instances)
+
+    data_train = data[I[1:n_train],:]
+    data_test = data[I[n_train+1:end],:]
 
     nams = names(iris)
 
-    g = getNet(1, data)
+    g = getNet(2, data_train)
 
-    C = [1,2,3]
+    C = collect(Set(data[:, :Species]))
+    display(C)
 
     s = 0
-    for i = 1:size(data, 1)
-        x = collect(data[i, 1:end-1])
-        L = map( c -> P_post(c, x, g, data), C )
+    for i = 1:size(data_test, 1)
+        x = collect(data_test[i, 1:end-1])
+        L = map( c -> P_post(c, x, g, data_train), C )
         c = argmax(L)
-        if c == data[i, end]
+        if C[c] == data_test[i, end]
             s += 1
         else
-            println( c, "  >>>>  ",  data[i, end])
+            println( c, "  >>>>  ",  data_test[i, end])
         end
     end
 
-    println(s / size(data, 1))
+    println(s / size(data_test, 1))
 
 
 
 end
 
-classification()
+@time classification()
 
