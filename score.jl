c2a91dcfa4e64abfab74c1c68e1acad2a8b1757b
Jesus Mejia <jesusmejded@gmail.com>
2020-01-06 14:55:04 -0600

    MDL score

---
+++ score.jl
@@ -0,0 +1,20 @@
+function MDL(BN_model)
+    fLogScore = 0.0
+
+    p = 1.0
+    for i = 1:size(BN_model.data, 1)
+        x = collect(BN_model.data[i, 1:end])
+        p *= P(x, BN_model)
+    end
+
+    fLogScore = -log(p)
+
+    for v in vertices(BN_model.graphInverted)
+
+        n = neighbors(BN_model.graphInverted, v)
+        m = BN_model.attributeNumValues[v] - 1
+        fLogScore += 0.5 * length(n) * m * log(size(BN_model.data, 2))
+    end
+
+    fLogScore
+end
